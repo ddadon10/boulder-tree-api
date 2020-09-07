5abd05b93439f62a912036a7dc8491119f0cb2be
David Dadon <david@dadon.fr>
2020-09-07 20:12:10 +0200

    Add Genus resource

---
+++ src/main/java/sh/david/bouldertreeapi/GenusResource.java
@@ -0,0 +1,34 @@
+package sh.david.bouldertreeapi;
+
+import java.util.TreeMap;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import sh.david.bouldertreeapi.datastore.Genus;
+
+@Path("/genus")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public class GenusResource {
+  TreeMap<String, Genus> genus = Main.DATASTORE.getGenusDatabase();
+
+  @GET
+  @Path("/")
+  public Response getGenus(){
+  Genus[] genusArray = genus.values().toArray(new Genus[0]);
+  return Response.ok().entity(genusArray).build();
+  }
+
+  @GET
+  @Path("/{name}")
+  public Response getGenus(@PathParam("name") String name){
+    Genus genusByName = genus.get(name);
+    if (genusByName == null) {
+      return Response.status(404).build();
+    }
+    return Response.ok().entity(genusByName).build();
+  }
+
+}
