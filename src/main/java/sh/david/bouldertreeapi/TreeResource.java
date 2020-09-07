50b30be87ee45c1e4c13e0752ff223fc28805701
David Dadon <david@dadon.fr>
2020-09-07 22:39:06 +0200

    Start working on queryparam filter

--- src/main/java/sh/david/bouldertreeapi/TreeResource.java
+++ src/main/java/sh/david/bouldertreeapi/TreeResource.java
@@ -5,8 +5,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import sh.david.bouldertreeapi.datastore.LeafCycle;
 import sh.david.bouldertreeapi.datastore.Tree;
 
 @Path("/trees")
@@ -17,8 +22,13 @@ public class TreeResource {
 
   @GET
   @Path("/")
-  public Response getTrees() {
+  public Response getTrees(@QueryParam("leafCycle") LeafCycle leafCycle, @Context UriInfo uriInfo) {
     Tree[] arrayTrees = trees.values().toArray(new Tree[0]);
+    MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+    if (leafCycle != null) {
+      ;
+    }
+
     return Response.ok().entity(arrayTrees).build();
   }
 
